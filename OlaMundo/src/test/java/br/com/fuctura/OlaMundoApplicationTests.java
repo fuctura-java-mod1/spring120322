a582761a478b9a31069f595d29e88d86b345992d
ericmoraess <ericmoraess@gmail.com>
2022-04-13 08:25:01 -0300

    aula 1

---
+++ OlaMundo/src/test/java/br/com/fuctura/OlaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fuctura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
